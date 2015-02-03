e5951b8b65aad688f4325a0c94ff096e0c812c25
Gasisin <andis1985@mail.ru>
2015-02-03 22:58:26 +0300

    add

---
+++ app/src/main/java/com/home/azot/shlyap/GameManager.java
@@ -0,0 +1,78 @@
+package com.home.azot.shlyap;
+
+import android.app.Activity;
+import android.content.Context;
+
+/**
+ * Created by Женя on 30.01.2015.
+ */
+public class GameManager {
+    private int _state;
+
+    public static GameManager instance;
+    public static TeamManager teamManager;
+//    public static TaskManager taskManager;
+
+    private int _teamCount = 0;
+
+    private GameUser _currentUser = null;
+    private Context _context;
+
+    public final static int TEAM_SELECT = 1;
+    public final static int FIRST_SELECT = 2;
+    public final static int GAME_STAGE = 3;
+
+    private int _currentStage = 0;
+
+    private CommandTask _currentTask = null;
+
+    public static GameManager getInstance()
+    {
+        if (instance == null)
+        {
+            instance = new GameManager();
+        }
+        return instance;
+    }
+
+    public CommandTask getCurrentTask(){
+        if (_currentTask==null&&_currentUser!=null){
+            _currentTask = getCurrentTeam().getTaskForUser(_currentUser);
+        }
+        if (_currentUser==null)
+            return null;
+        return _currentTask;
+    }
+
+    private GameManager(){
+        _state = TEAM_SELECT;
+        teamManager = new TeamManager();
+    }
+
+    public void setTeamCount(){
+        _teamCount = teamManager.getAddedUserCount()/2;
+    }
+    public TeamManager getTeamManager(){
+        return teamManager;
+    }
+    public int getGameState(){
+        return _state;
+    }
+    public Team getCurrentTeam(){
+            return teamManager.getTeamInGame().get(_currentStage%_teamCount);
+    }
+
+    public void setCurrentUser(GameUser currentUser){
+        _currentUser = currentUser;
+    }
+
+    public GameUser getCurrentUser(){
+        return _currentUser;
+    }
+    public void setGameState(int state){
+        _state = state;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/home/azot/shlyap/GameScreen.java
@@ -0,0 +1,290 @@
+package com.home.azot.shlyap;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Handler;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class GameScreen extends ActionBarActivity {
+
+    public static TaskManager taskManager;
+    GameManager gameManager;
+    TeamManager teamManager;
+    MyDBHelper dbHelper;
+    TextView text;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_screen);
+
+        try {
+            taskManager = new TaskManager(this.getApplicationContext());
+            gameManager = GameManager.getInstance();
+            teamManager = gameManager.getTeamManager();
+            dbHelper = new MyDBHelper(this);
+            setStartScreen();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+//        text = (TextView)findViewById(R.id.textView1);
+//        text.setText(taskManager.getOneWord().toString());
+
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        Log.d("MyTag", "Back");
+    }
+
+    private void setStartScreen() {
+
+        Button btnAzoT = (Button) findViewById(R.id.Azot);
+        Button btnKate = (Button) findViewById(R.id.Kate);
+        Button btnUrii = (Button) findViewById(R.id.Urii);
+        Button btnKristina = (Button) findViewById(R.id.Kris);
+        Button btnVera = (Button) findViewById(R.id.Vera);
+        Button btnSlava = (Button) findViewById(R.id.Slava);
+
+        Button startBtn = (Button) findViewById(R.id.StartGameBtn);
+
+
+        final Button resetBtn = (Button) findViewById(R.id.resetBtn);
+
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGame();
+            }
+        });
+
+        resetBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reset();
+            }
+        });
+        View.OnClickListener oclBtn = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.Azot:
+                        teamManager.addUserInTeam(teamManager.getUserById(1));
+                        setTextAboutCommand();
+                        break;
+                    case R.id.Kate:
+                        teamManager.addUserInTeam(teamManager.getUserById(2));
+                        setTextAboutCommand();
+                        break;
+                    case R.id.Urii:
+                        teamManager.addUserInTeam(teamManager.getUserById(3));
+                        setTextAboutCommand();
+                        break;
+                    case R.id.Kris:
+                        teamManager.addUserInTeam(teamManager.getUserById(4));
+                        setTextAboutCommand();
+                        break;
+                    case R.id.Vera:
+                        teamManager.addUserInTeam(teamManager.getUserById(5));
+                        setTextAboutCommand();
+                        break;
+                    case R.id.Slava:
+                        teamManager.addUserInTeam(teamManager.getUserById(6));
+                        setTextAboutCommand();
+                        break;
+                }
+                setButtonInactive(v);
+            }
+        };
+
+        btnAzoT.setOnClickListener(oclBtn);
+        btnKate.setOnClickListener(oclBtn);
+        btnUrii.setOnClickListener(oclBtn);
+        btnKristina.setOnClickListener(oclBtn);
+        btnVera.setOnClickListener(oclBtn);
+        btnSlava.setOnClickListener(oclBtn);
+
+    }
+
+    private void startGame() {
+        if (teamManager.isFullTeam()){
+            choiseWhoFirst();
+        }
+    else{
+            Context context = getApplicationContext();
+            CharSequence text = "Неверное колличество игроков в командах";
+            int duration = Toast.LENGTH_SHORT;
+
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();}
+    }
+
+    private void setButtonInactive(View view){
+        view.setEnabled(false);
+        view.setClickable(false);
+
+
+    }
+
+    private void choiseWhoFirst(){
+        gameManager.setTeamCount();  //получает колличество команд в игре
+
+        setContentView(R.layout.who_first_screen);
+
+        Team currentTeam  = gameManager.getCurrentTeam();
+
+        Button firstBtn = (Button) findViewById(R.id.first);
+        Button secondBtn = (Button) findViewById(R.id.second);
+
+        firstBtn.setText(String.valueOf(currentTeam.getGameUser(Team.FIRST_USER).getUserName()));
+        secondBtn.setText(String.valueOf(currentTeam.getGameUser(Team.SECOND_USER).getUserName()));
+
+        View.OnClickListener choseFirstn = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                switch (v.getId()) {
+                    case R.id.first:
+                        gameManager.setCurrentUser(gameManager.getCurrentTeam().getGameUser(Team.FIRST_USER));
+                        setButtonActive(findViewById(R.id.second));
+                        break;
+                    case R.id.second:
+                        gameManager.setCurrentUser(gameManager.getCurrentTeam().getGameUser(Team.SECOND_USER));
+                        setButtonActive(findViewById(R.id.first));
+                        break;
+                }
+                setButtonInactive(v);
+            }
+        };
+        firstBtn.setOnClickListener(choseFirstn);
+        secondBtn.setOnClickListener(choseFirstn);
+
+        Button startBtn = (Button) findViewById(R.id.chekedwhofirstbtn);
+        startBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startGuestWord();
+            }
+        });
+    }
+    private void setButtonActive(View view){
+        view.setEnabled(true);
+        view.setClickable(true);
+    }
+
+    private void startGuestWord(){
+        setContentView(R.layout.game_screen);
+        MyTimer myTimer = new MyTimer();
+
+        Handler handler = new Handler(){
+            public void handleMessage(android.os.Message msg) {
+                // обновляем TextView
+                Log.d("MyTag", "Handler mes - "+msg.what);
+                TextView timer = (TextView) findViewById(R.id.timer);
+                timer.setText(String.valueOf(msg.what));
+            };
+        };
+        myTimer.setHandler(handler);
+        myTimer.start();
+
+        setNextWord();
+        Log.d("MyTag", "GameStart");
+    }
+
+
+
+    private void setNextWord() {
+        TextView word = (TextView) findViewById(R.id.taskWord);
+        final TaskWord tWord = gameManager.getCurrentTask().getNextWord();
+        if (tWord==null){
+            showRoundResult();
+        }
+        else {
+            tWord.startGuess();
+
+            word.setText(tWord.getWord());
+
+            Button guesedBtn = (Button) findViewById(R.id.checkedBtn);
+            Button missedBtn = (Button) findViewById(R.id.missedBtn);
+
+            View.OnClickListener someWordAction = new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    switch (v.getId()) {
+                        case R.id.checkedBtn:
+                            tWord.setGuessed();
+                            break;
+                        case R.id.missedBtn:
+                            tWord.miss();
+                            break;
+                    }
+                    setNextWord();
+                }
+            };
+            guesedBtn.setOnClickListener(someWordAction);
+            missedBtn.setOnClickListener(someWordAction);
+        }
+    }
+
+    private void showRoundResult() {
+        Context context = getApplicationContext();
+        CharSequence text = "Все слова угаданы";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    private void setTextAboutCommand(){
+        int _count = 0;
+        LinearLayout llMain = (LinearLayout) findViewById(R.id.CommanDescription);
+        int userCount = teamManager.getAddedUserCount();
+        ArrayList<GameUser> addedUsers = teamManager.getAddedUser();
+        llMain.removeAllViews();
+        Iterator<GameUser> itr = addedUsers.iterator();
+        while (itr.hasNext()) {
+            if (_count%2==0){
+                TextView tef = new TextView(this);
+                tef.setText("Команда:");
+                llMain.addView(tef);
+             }
+            _count++;
+            TextView tef = new TextView(this);
+            tef.setText(itr.next().getUserName().toString());
+            llMain.addView(tef);
+//            taskWordArrayList.add(new TaskWord(allWord.get(itr.next())));
+        }
+
+
+    }
+
+    private void reset(){
+        LinearLayout linMain = (LinearLayout) findViewById(R.id.CommanDescription);
+        linMain.removeAllViews();
+        teamManager.resetTeam();
+        GridLayout llMain = (GridLayout) findViewById(R.id.gridLayout);
+        for (int i = 0; i < llMain.getChildCount(); i++) {
+            View view = llMain.getChildAt(i);
+            setButtonActive(view);
+        }
+    }
+
+
+}
