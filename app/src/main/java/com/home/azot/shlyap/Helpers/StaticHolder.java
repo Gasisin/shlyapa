57154d6509dd1d605f70fc310eba078acb321fb8
Gasisin <andis1985@mail.ru>
2015-02-19 20:55:15 +0300

    fwe

--- app/src/main/java/com/home/azot/shlyap/Helpers/StaticHolder.java
+++ app/src/main/java/com/home/azot/shlyap/Helpers/StaticHolder.java
@@ -1,6 +1,9 @@
 package com.home.azot.shlyap.Helpers;
 
 import com.home.azot.shlyap.Activities.UserSelectActivity;
+import com.home.azot.shlyap.GameManager;
+import com.home.azot.shlyap.TaskManager;
+import com.home.azot.shlyap.TeamManager;
 
 /**
  * Created by Женя on 16.02.2015.
@@ -8,4 +11,7 @@ import com.home.azot.shlyap.Activities.UserSelectActivity;
 public class StaticHolder {
 
     public static UserSelectActivity userSelectActivityInstance;
+    public static TaskManager taskManager;
+    public static GameManager gameManager;
+    public static TeamManager teamManager;
 }
